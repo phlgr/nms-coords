133363f904d0b93194f4330411868a43f2731da3
phlgr <philipp@gartz.dev>
2022-05-20 22:22:53 +0200

    ✨ Create first draft of Details Page

--- pages/coordinates/[id]/index.tsx
+++ pages/coordinates/[id]/index.tsx
@@ -1,7 +1,19 @@
-import { Card, Container } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Card,
+  Container,
+  Group,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
 import { ObjectId } from "mongodb";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { Activity, ArrowLeft, GitMerge, Versions } from "tabler-icons-react";
 import clientPromise from "../../../lib/mongodb";
 import { Coordinate } from "../../../types/coordinates";
 
@@ -10,8 +22,18 @@ type CoordinatePageProps = {
 };
 
 const CoordinatePage = ({ coordinate }: CoordinatePageProps) => {
+  const router = useRouter();
+  const theme = useMantineTheme();
   return (
     <Container>
+      <Button
+        variant="subtle"
+        onClick={() => router.back()}
+        style={{ marginBottom: theme.spacing.md }}
+      >
+        <ArrowLeft />
+        Back to Home
+      </Button>
       <Card>
         <Card.Section>
           <Image
@@ -22,6 +44,18 @@ const CoordinatePage = ({ coordinate }: CoordinatePageProps) => {
             alt=""
           />
         </Card.Section>
+        <Stack style={{ marginTop: theme.spacing.md }}>
+          <Group position="apart">
+            <Group>
+              <Title order={3}>{coordinate.type}</Title>
+              <Badge>{coordinate.galaxy}</Badge>
+            </Group>
+            <Badge>
+              <Activity size={14} /> {coordinate.version}
+            </Badge>
+          </Group>
+          <Text>{coordinate.title}</Text>
+        </Stack>
       </Card>
     </Container>
   );
