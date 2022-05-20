47be99bb96056dcb58e76dfee21731c3953193a7
phlgr <philipp@gartz.dev>
2022-05-20 23:23:36 +0200

    ♻️ Make whole card clickable

--- components/CoordinateCard.tsx
+++ components/CoordinateCard.tsx
@@ -28,37 +28,37 @@ const CoordinateCard = ({
   const theme = useMantineTheme();
 
   return (
-    <Card style={{ display: "flex", flexDirection: "column" }}>
-      <Card.Section>
-        <Image
-          alt=""
-          height={170}
-          width={300}
-          objectFit="cover"
-          sizes="40vw"
-          src={img}
-        />
-      </Card.Section>
-      <Group
-        position="apart"
-        style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-      >
-        <Title order={3}>{type}</Title>
-        <Badge>{galaxy}</Badge>
-      </Group>
+    <Link href={`/coordinates/${id}`} passHref>
+      <Card style={{ display: "flex", flexDirection: "column" }}>
+        <Card.Section>
+          <Image
+            alt=""
+            height={170}
+            width={300}
+            objectFit="cover"
+            sizes="40vw"
+            src={img}
+          />
+        </Card.Section>
+        <Group
+          position="apart"
+          style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+        >
+          <Title order={3}>{type}</Title>
+          <Badge>{galaxy}</Badge>
+        </Group>
 
-      <Text style={{ flexGrow: 1 }}>{desc}</Text>
-      <Link href={`/coordinates/${id}`} passHref>
+        <Text style={{ flexGrow: 1 }}>{desc}</Text>
+
         <Button
-          component="a"
           variant="light"
           fullWidth
           style={{ marginTop: theme.spacing.md }}
         >
           Show Details
         </Button>
-      </Link>
-    </Card>
+      </Card>
+    </Link>
   );
 };
 
