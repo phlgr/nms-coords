b868a4df7c21c3af835c47d09f173e9a552b68ce
phlgr <philipp@gartz.dev>
2022-05-20 14:00:26 +0200

    🔥 Remove boilerplate code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { Container } from "@mantine/core";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return <Container></Container>;
